fbc8e7ea531ef72196145a9fd7245be9e5e886dd
alexdriaguine <alexdriagin12@gmail.com>
2018-05-20 01:22:02 +0200

    user concat in template for consistency

--- lib/template.ts
+++ lib/template.ts
@@ -10,10 +10,10 @@ export function insertInterfaceTemplateIntoFileContent(
   const lines = fileContent.split('\n')
   const imports = lines.filter(l => l.includes('import') && l.includes('from'))
   const indexOfLastImport = lines.indexOf(imports[imports.length - 1])
-  const firstPart = lines.slice(0, indexOfLastImport + 1)
+  const start = lines.slice(0, indexOfLastImport + 1)
   const middle = template(props).split('\n')
   const end = lines.slice(indexOfLastImport + 1)
-  const all = [...firstPart, ...middle, ...end].join('\n').toString()
+  const all = start.concat(middle).concat(end).join('\n').toString()
 
   return all
 }
