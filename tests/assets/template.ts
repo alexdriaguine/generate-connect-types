f41a2934b85065cc00bb3a52176a540060d8840a
alexdriaguine <alexdriagin12@gmail.com>
2018-05-24 22:00:48 +0200

    use prettier to format the integration test files

--- tests/assets/template.ts
+++ tests/assets/template.ts
@@ -1,8 +1,15 @@
+import * as prettier from 'prettier'
+
 export const connectedProps = [
   {name: 'index', value: 'state.index', type: 'number'},
   {name: 'name', value: 'state.name', type: 'string'},
   {name: 'ble', value: 'state.bla', type: 'Bla'},
   {name: 'hoho', value: 33, type: 'number'},
+  {
+    name: 'nested',
+    value: 'state.nested',
+    type: '{nested: {as: {fudge: true}}}',
+  },
 ]
 
 export const connectedDispatch = [
@@ -15,7 +22,12 @@ export const expectedInterface = `interface ConnectProps {
 }
 `
 
-export const template = `import {connect} from 'react-redux'
+export const templateInferred = prettier.format(
+  `import {
+  connect as _connect,
+  InferableComponentEnhancerWithProps,
+} from 'react-redux'
+import {MapDispatchToProps, MergeProps, Options} from 'react-redux'
 
 type Bla = {
   foo: string
@@ -24,6 +36,11 @@ interface State {
   index: number
   name: string
   bla: Bla
+  nested: {
+    as: {
+      fudge: boolean
+    }
+  }
 }
 const state: State = {
   index: 1,
@@ -31,6 +48,67 @@ const state: State = {
   bla: {
     foo: 'asd',
   },
+  nested: {
+    as: {
+      fudge: true
+    }
+  }
+}
+
+type MapStateToProps = (state: State) => any
+
+const connect = function(
+  mapStateToProps?: MapStateToProps,
+  mapDispatchToProps?: MapDispatchToProps<any, any>,
+  mergeProps?: MergeProps<any, any, any, any>,
+  options?: Options,
+): InferableComponentEnhancerWithProps<any, any> {
+  return _connect.apply(undefined, arguments)
+}
+
+const Comp = props => <div>hehe</div>
+
+const t = connect(
+  (state: State) => ({
+    ${connectedProps.map(p => `${p.name}: ${p.value}`).join(',\n')}
+  }),
+  () => ({
+    ${connectedDispatch.map(p => `${p.name}: ${p.value}`).join(',\n')}
+  }),
+)(Comp)
+`,
+  {parser: 'typescript'},
+)
+
+export const template = prettier.format(
+  `
+import {connect} from 'react-redux'
+import * as prettier from 'prettier';
+
+type Bla = {
+  foo: string
+}
+interface State {
+  index: number
+  name: string
+  bla: Bla
+  nested: {
+    as: {
+      fudge: boolean
+    }
+  }
+}
+const state: State = {
+  index: 1,
+  name: 'bla',
+  bla: {
+    foo: 'asd',
+  },
+  nested: {
+    as: {
+      fudge: true
+    }
+  }
 }
 
 const Comp = (props) => <div>hehe</div>
@@ -42,4 +120,6 @@ const t = connect(
   () => ({
     ${connectedDispatch.map(p => `${p.name}: ${p.value}`).join(',\n')}
   }),
-)(Comp)`
+)(Comp)`,
+  {parser: 'typescript'},
+)
