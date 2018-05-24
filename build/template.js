e427e950ae0eefad57f326c56a261e85ca3bcf26
alexdriaguine <alexdriagin12@gmail.com>
2018-05-24 22:01:38 +0200

    check in build

--- build/template.js
+++ build/template.js
@@ -1,17 +1,29 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.template = (props) => `
-interface ConnectProps {
-  ${props.join('\n  ')}
-}`;
+const prettier = require("prettier");
+exports.template = (props) => {
+    const interfaceTemplate = `
+    interface ConnectProps {
+      ${props.map(prop => {
+        return `${prop.propName}: ${prop.typeName}\n`;
+    })}
+    }
+  `;
+    return prettier.format(interfaceTemplate);
+};
 function insertInterfaceTemplateIntoFileContent(fileContent, props) {
     const lines = fileContent.split('\n');
     const imports = lines.filter(l => l.includes('import') && l.includes('from'));
-    const indexOfLastImport = lines.indexOf(imports[imports.length - 1]);
-    const firstPart = lines.slice(0, indexOfLastImport + 1);
+    const indexOfLastImport = lines.lastIndexOf(imports[imports.length - 1]);
+    const start = lines.slice(0, indexOfLastImport + 1);
     const middle = exports.template(props).split('\n');
     const end = lines.slice(indexOfLastImport + 1);
-    const all = [...firstPart, ...middle, ...end].join('\n').toString();
+    const all = start
+        .concat(['\n']) // For a space between imports and our generated interface
+        .concat(middle)
+        .concat(end)
+        .join('\n')
+        .toString();
     return all;
 }
 exports.insertInterfaceTemplateIntoFileContent = insertInterfaceTemplateIntoFileContent;
--- build/type-checker.js
+++ build/type-checker.js
@@ -1,29 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const ts = require("typescript");
-const utils_1 = require("./utils");
-function mapToPropArray(res) {
-    return res
-        .replace(/{|}/g, '')
-        .split(';')
-        .map(r => r.trim())
-        .filter(r => r);
-}
 function isArrowFunction(node) {
     return node.kind === ts.SyntaxKind.ArrowFunction;
 }
-function isJsxElement(node) {
+function isJsxComponentDeclaration(node) {
     return node.getChildren().some(n => n.kind === ts.SyntaxKind.JsxElement);
 }
 function isConnectChild(node) {
     return node.parent.getFullText().includes('connect');
 }
-function getProps(results) {
-    return results.map(mapToPropArray).reduce(utils_1.flatten, []);
-}
-exports.getProps = getProps;
 function isConnectArrowFunctionArgument(node) {
-    return isArrowFunction(node) && !isJsxElement(node) && isConnectChild(node);
+    return (isArrowFunction(node) &&
+        !isJsxComponentDeclaration(node) &&
+        isConnectChild(node));
 }
 exports.isConnectArrowFunctionArgument = isConnectArrowFunctionArgument;
 function isObjectType(flags) {
@@ -34,16 +24,22 @@ function getTypeInformationFromNode(node, typeChecker) {
     const results = [];
     function visitNode(node) {
         if (isConnectArrowFunctionArgument(node)) {
-            const sym = typeChecker.getTypeAtLocation(node).symbol;
-            const type = typeChecker.getTypeOfSymbolAtLocation(sym, sym.valueDeclaration);
+            const symbol = typeChecker.getTypeAtLocation(node).symbol;
+            const type = typeChecker.getTypeOfSymbolAtLocation(symbol, symbol.valueDeclaration);
             const returnType = type.getCallSignatures()[0].getReturnType();
             if (isObjectType(returnType.flags)) {
-                results.push(typeChecker.typeToString(returnType));
+                const returnTypeProperties = returnType.getProperties();
+                returnTypeProperties.forEach(prop => {
+                    const type = typeChecker.getTypeOfSymbolAtLocation(prop, prop.valueDeclaration);
+                    const name = prop.getName();
+                    const typeName = typeChecker.typeToString(type);
+                    results.push({ propName: name, typeName });
+                });
             }
         }
         ts.forEachChild(node, visitNode);
     }
     visitNode(node);
-    return getProps(results);
+    return results;
 }
 exports.getTypeInformationFromNode = getTypeInformationFromNode;
