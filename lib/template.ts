57f981eb56b2cf7322cce94fc1aeaa733c83b64c
alexdriaguine <alexdriagin12@gmail.com>
2018-05-24 21:57:11 +0200

    user prettier in template so the interface does not look like shit

--- lib/template.ts
+++ lib/template.ts
@@ -1,19 +1,34 @@
-export const template = (props: string[]) => `
-interface ConnectProps {
-  ${props.join('\n  ')}
-}`
+import {Prop} from './type-checker'
+import * as prettier from 'prettier'
+
+export const template = (props: Array<Prop>) => {
+  const interfaceTemplate = `
+    interface ConnectProps {
+      ${props.map(prop => {
+        return `${prop.propName}: ${prop.typeName}\n`
+      })}
+    }
+  `
+
+  return prettier.format(interfaceTemplate)
+}
 
 export function insertInterfaceTemplateIntoFileContent(
   fileContent: string,
-  props: Array<string>,
+  props: Array<Prop>,
 ) {
   const lines = fileContent.split('\n')
   const imports = lines.filter(l => l.includes('import') && l.includes('from'))
-  const indexOfLastImport = lines.indexOf(imports[imports.length - 1])
+  const indexOfLastImport = lines.lastIndexOf(imports[imports.length - 1])
   const start = lines.slice(0, indexOfLastImport + 1)
   const middle = template(props).split('\n')
   const end = lines.slice(indexOfLastImport + 1)
-  const all = start.concat(middle).concat(end).join('\n').toString()
+  const all = start
+    .concat(['\n']) // For a space between imports and our generated interface
+    .concat(middle)
+    .concat(end)
+    .join('\n')
+    .toString()
 
   return all
 }
